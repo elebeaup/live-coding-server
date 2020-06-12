1ca6b0f5b41f18662a0c102982dc3dcb7301c241
Emmanuel Lebeaupin <elebeaup@users.noreply.github.com>
2020-06-12 18:21:17 +0200

    Add xterm.js demo

---
+++ demo/js/app.js
@@ -0,0 +1,88 @@
+import {FitAddon} from '/web_modules/xterm-addon-fit.js';
+import Terminal from '/web_modules/xterm.js';
+
+const Command = {
+  OUTPUT: '0',
+  SET_WINDOW_TITLE: '1',
+  SET_PREFERENCES: '2',
+  INPUT: '0',
+  RESIZE_TERMINAL: '1'
+};
+
+class Xterm {
+  constructor() {
+    this.textEncoder = new TextEncoder();
+    this.textDecoder = new TextDecoder();
+    this.fitAddon = new FitAddon();
+    this.connect();
+    this.openTerminal();
+    window.addEventListener('resize', this.onWindowResize.bind(this));
+  }
+
+  openTerminal() {
+    this.terminal = new Terminal.Terminal();
+    const {fitAddon, terminal} = this;
+    terminal.loadAddon(fitAddon);
+    terminal.open(document.getElementById('terminal-container'));
+    fitAddon.fit();
+    terminal.onData(this.onTerminalData.bind(this));
+    terminal.onResize(this.onTerminalResize.bind(this));
+  }
+
+  connect() {
+    this.socket = new WebSocket('ws://localhost:3000/xterm');
+    const {socket} = this;
+    socket.binaryType = 'arraybuffer';
+    socket.addEventListener('open', this.onSocketOpen.bind(this));
+    socket.addEventListener('message', this.onSocketData.bind(this));
+  }
+
+  onTerminalData(data) {
+    const {socket, textEncoder} = this;
+    if (socket.readyState === WebSocket.OPEN) {
+      socket.send(textEncoder.encode(Command.INPUT + data));
+    }
+  }
+
+  onTerminalResize({cols: columns, rows}) {
+    const {socket, textEncoder} = this;
+    if (socket.readyState === WebSocket.OPEN) {
+      const msg = JSON.stringify({
+        columns,
+        rows
+      });
+      socket.send(textEncoder.encode(Command.RESIZE_TERMINAL + msg));
+    }
+  }
+
+  onSocketOpen() {
+  }
+
+  onSocketData() {
+    const {terminal, textDecoder, textEncoder} = this;
+    const rawData = textDecoder.decode(event.data);
+    const cmd = rawData[0];
+    const data = rawData.slice(1);
+    switch (cmd) {
+      case Command.SET_PREFERENCES: {
+        const preferences = JSON.parse(data);
+        Object.keys(preferences).forEach((key) => {
+          terminal.setOption(key, preferences[key]);
+        });
+        break;
+      }
+      case Command.OUTPUT: {
+        terminal.write(textEncoder.encode(data));
+        break;
+      }
+    }
+  }
+
+  onWindowResize() {
+    this.fitAddon.fit();
+  }
+}
+
+(function () {
+  new Xterm();
+})();
---
+++ demo/web_modules/xterm/lib/xterm.js
@@ -0,0 +1,2 @@
+import '../../common/_commonjsHelpers-51a1c497.js';
+export { default } from '../../xterm.js';
